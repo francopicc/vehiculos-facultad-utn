2529e22eacb24219647f292f8061350fede220fd
Franco Ignacio Piccirilli <francopiccirilli51@gmail.com>
2023-01-19 00:01:04 -0300

    Mini-Fix: Hover Ofertas y Scroll en Ofertas

--- vehiculos/static/js/home.js
+++ vehiculos/static/js/home.js
@@ -1,2 +1,9 @@
 // Solo destinado a la funcionalidad web de la pagina home "/"
 
+// Boton Home Scrolling a Ofertas Destacadas
+
+const boton = document.querySelector(".seeOffer");
+
+boton.addEventListener('click', () => {
+    document.querySelector(".ofertasTitle").scrollIntoView();
+})
