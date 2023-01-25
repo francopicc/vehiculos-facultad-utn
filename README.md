867ef47fa65b9b825a5f08b6a8edcdfcc58f2b3e
Franco Ignacio Piccirilli <francopiccirilli51@gmail.com>
2023-01-25 01:37:42 -0300

    Añadido mySQL

--- README.md
+++ README.md
@@ -7,4 +7,9 @@ cd vehiculo
 python3 manage.py runserver
 ```
 
+## Algunos Tips
 Recomiendo antes de probar el proyecto, iniciarlo con conectividad a internet ya que posee cdn's que no provienen localmente (fuentes, etc).
+
+Tambien puede que si estas editando el proyecto y no se muestran los cambios pueden ser por dos razones: no recargaste la pagina o solo te esta mostrando la pagina en cache (solucion: shift + f5)
+
+El servidor con la base de datos esta en Xampp (herramienta que ya incluye mySQL, junto a otras herramientas como Apache)
--- vehiculos/models.py
+++ vehiculos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Vuelo (models.Model):
+    origenVuelo = models.CharField(max_length=30, verbose_name='Origen')
+    destinoVuelo = models.CharField(max_length=30, verbose_name='Destino')
+    # Este queda medio dudoso, pero lo vamos a dejar aca :D
+    fechaIngreso = models.DateField(verbose_name='Ingreso')
+    fechaEgreso = models.DateField(verbose_name='Egreso')
+    opcionVuelo = models.CharField(verbose_name='Opciones', max_length=15)
+    # Este tambien anda medio medio...
+    cantidadPersonas = models.IntegerField(verbose_name='Cantidad')
