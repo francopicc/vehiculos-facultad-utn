3f5ec1186d47293b40501967d2ddfb111898cb05
Franco Ignacio Piccirilli <francopiccirilli51@gmail.com>
2023-01-25 02:28:42 -0300

    Fix Modelos y algunos comentarios

--- vehiculos/models.py
+++ vehiculos/models.py
@@ -2,12 +2,37 @@ from django.db import models
 
 # Create your models here.
 
+# Modelo para recibir POST de alquileres, etc.
+
 class Vuelo (models.Model):
+    # Se podria añadir un ID aca porque es mas facil identificar el vuelo (pronto)
     origenVuelo = models.CharField(max_length=30, verbose_name='Origen')
     destinoVuelo = models.CharField(max_length=30, verbose_name='Destino')
     # Este queda medio dudoso, pero lo vamos a dejar aca :D
-    fechaIngreso = models.DateField(verbose_name='Ingreso')
-    fechaEgreso = models.DateField(verbose_name='Egreso')
+    fechaIngreso = models.DateField(verbose_name='Ingreso', null=False)
+    fechaEgreso = models.DateField(verbose_name='Egreso', null=False)
     opcionVuelo = models.CharField(verbose_name='Opciones', max_length=15)
     # Este tambien anda medio medio...
     cantidadPersonas = models.IntegerField(verbose_name='Cantidad')
+
+    def __str__(self):
+        fila = self.origenVuelo + " " + self.destinoVuelo
+        return fila
+
+# Modelo para recibir cada dato del usuario. Pueden encontrarse superusuarios y usuarios.
+
+# Arreglar comillas doble por una comilla (mas simple)
+
+class User (models.Model):
+    nombre = models.CharField(verbose_name='Nombre', max_length=30)
+    apellido = models.CharField(verbose_name='Apellido', max_length=30)
+    nacimiento = models.DateField(verbose_name="Nacimiento")
+    telefono = models.IntegerField(verbose_name='Telefono', max_length=20)
+    tipodocumento = models.CharField(verbose_name='Tipo DNI', max_length=15)
+    documento = models.IntegerField(verbose_name="Documento", max_length=35)
+    pais = models.CharField(verbose_name="Pais", max_length=40)
+    provincia = models.CharField(verbose_name="Provincia", max_length=25)
+    calle = models.CharField(verbose_name="Calle", max_length=40)
+    altura = models.CharField(verbose_name="Altura", max_length=20)
+    ciudad = models.CharField(verbose_name="Ciudad", max_length=30)
+    cp = models.CharField(verbose_name="Codigo Postal", max_length=10)
